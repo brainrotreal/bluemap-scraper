51529c6f9c290bdd2c4474f96e93e1571744f2e5
Daskey_bruhh <60326436+brainrotreal@users.noreply.github.com>
2026-06-16 20:36:34 -0300

    Test commit vercel maybe fix

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 import requests
+import isEven
+
+#test
 
 limiter = Limiter(key_func=get_remote_address)
 app = fastapi.FastAPI()
